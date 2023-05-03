3d9174e7cc7e37dff395eb4a492e24da4794041f
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2023-05-03 18:43:30 -0500

    Fade RGB values with incident data

--- color.js
+++ color.js
@@ -1,7 +1,7 @@
 const map = new Object()
-map.data = {}
+map.data = 0;
 function init() {
-    console.log('init')
+    console.log('init');
     // this will not work by opening html from local file
     // use npm module http-server instead
     fetch('years_states_named.json') // this is the input json file for the data
@@ -9,8 +9,13 @@ function init() {
     .then(data => {
         map.data = data;
         updateColors();
+        map.maxIncidents = getMax('incidents');
+        map.maxMiles = getMax('miles');
     })
-  .catch(error => console.error(error));    
+    .catch(error => console.error(error));    
+    // compute maximum values occurring in data set
+    console.log(map.data);
+    
     // set year slider to 2022
     document.querySelector('#yearSlider').value = 2022;
     console.log("Slider set.")
@@ -35,16 +40,31 @@ function updateColors() {
     // change fill color for states that appear in data
     for (const [stateName, values] of Object.entries(counties)) {
         try {
-            for (const state of svgDoc.querySelectorAll(`#${stateName} path`)) {
-                state.style.fill('red');
+            let red = (parseInt(map.data[year][stateName]['incidents']) / map.maxIncidents) * 255;
+            for (const county of svgDoc.querySelectorAll(`#${stateName.trim()} path`)) {
+                county.style.fill = `rgb(255, ${255 - red}, ${255 - red})`;
             }
             // svgDoc.querySelector(`#c${county}`).style.fill = 'red';
         }
         catch(TypeError) {
-            console.log(`County ${stateName} not found for ${year}.`)
+            console.log(`Region ${stateName} not found for ${year}.`)
         }
             
     }
+}
+
+// Get maximum occurring count of incidents/miles
+function getMax(key) {
+    let max = 0;
+    for (const [year, yearValues] of Object.entries(map.data)) {
+        for (const [stateCounty, stateCountyValues] of Object.entries(yearValues)) {
+            let count = +map.data[+year][stateCounty][key];
+            if (count > max) {
+                max = count;
+            }
+        }
+    }
+    return max;
 }
 
 init();
